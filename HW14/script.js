e6b2034426b9ab033ada9d6e71e4af7652a17305
ImEugene33 <murzakoy@gmail.com>
2021-07-18 00:00:12 +0300

    applied HW14

--- HW14/script.js
+++ HW14/script.js
@@ -1,43 +1,85 @@
-// const hamburger = new Hamburger(SIZE_SMALL);
-// // добавка из майонеза
-// hamburger.addTopping(TOPPING_MAYO);
-// hamburger.addTopping(TOPPING_POTATO);
+const TOPPINGS = {
+  cheese: { price: 15, ccal: 90 },
+  salad: { price: 10, ccal: 20 },
+  potato: { price: 10, ccal: 40 },
+  spices: { price: 15, ccal: 0 },
+  mayo: { price: 10, ccal: 85 },
+  salmon: { price: 50, ccal: 65 },
+  bacon: { price: 20, ccal: 30 },
+};
 
-// console.log("Price with sauce: “ + hamburger.getPrice());
-// console.log("Callories with sauce: “ + hamburger.getCallories());
-
-// маленький (50 тугриков, 20 калорий)
-
-// средний (75 тугриковб 30 каллорий)
-
-// большой (100 тугриков, 40 калорий)
-
-// Гамбургер может быть с одним из нескольких видов начинок:
-
-// сыром (+ 10 тугриков, + 20 калорий)
-
-// салатом (+ 20 тугриков, + 5 калорий)
-
-// картофелем (+ 15 тугриков, + 10 калорий)
-
-// посыпать приправой (+ 15 тугриков, 0 калорий)
-
-// полить майонезом (+ 20 тугриков, + 5 калорий).
-
-// При этом начинок можно добавить несколько или не быть совсем
-
-// Напишите программу, расчитывающую стоимость и калорийность гамбургера. Используй ООП подход (подсказка: нужен класс Гамбургер, константы, методы для выбора опций и рассчета нужных величин).
-
-class Hamburger {
-  constructor(price, calories) {
-    this.price = price;
-    this.calories = calories;
-  }
+function Burger(size) {
+  this.toppings = [];
+  this.properties = {
+    small: { price: 50, ccal: 150 },
+    medium: { price: 75, ccal: 200 },
+    large: { price: 90, ccal: 400 },
+  };
+  const sizeProp = this.properties[size];
+  this.price = sizeProp ? sizeProp.price : 0;
+  this.ccal = sizeProp ? sizeProp.ccal : 0;
 }
-const topings = [
-  new Cheese(10, 20),
-  new Salad(20, 5),
-  new Potato(15, 10),
-  new Spices(15, 0),
-  new Mayo(20, 5),
-];
+
+Burger.prototype.getPrice = function () {
+  return this.price + this.toppings.reduce((acc, e) => acc + e.price, 0);
+};
+
+Burger.prototype.getCal = function () {
+  return this.ccal + this.toppings.reduce((acc, e) => acc + e.ccal, 0);
+};
+
+Burger.prototype.addTopping = function (topping) {
+  this.toppings.push(topping);
+};
+
+function CheeseBurger(size) {
+  this.properties = {
+    small: { price: 60, ccal: 175 },
+    medium: { price: 85, ccal: 220 },
+    large: { price: 100, ccal: 450 },
+  };
+  const sizeProp = this.properties[size];
+  this.price = sizeProp ? sizeProp.price : 0;
+  this.ccal = sizeProp ? sizeProp.ccal : 0;
+}
+CheeseBurger.prototype = new Burger();
+
+function FishBurger(size) {
+  this.properties = {
+    small: { price: 85, ccal: 190 },
+    medium: { price: 100, ccal: 240 },
+    large: { price: 130, ccal: 470 },
+  };
+  const sizeProp = this.properties[size];
+  this.price = sizeProp ? sizeProp.price : 0;
+  this.ccal = sizeProp ? sizeProp.ccal : 0;
+}
+FishBurger.prototype = new Burger();
+
+function BigMacBacon(size) {
+  this.properties = {
+    small: { price: 75, ccal: 185 },
+    medium: { price: 90, ccal: 230 },
+    large: { price: 115, ccal: 460 },
+  };
+  const sizeProp = this.properties[size];
+  this.price = sizeProp ? sizeProp.price : 0;
+  this.ccal = sizeProp ? sizeProp.ccal : 0;
+}
+BigMacBacon.prototype = new Burger();
+
+const burgerOrder = new BigMacBacon("large");
+burgerOrder.addTopping(TOPPINGS.cheese);
+burgerOrder.addTopping(TOPPINGS.spices);
+burgerOrder.addTopping(TOPPINGS.bacon);
+
+console.log("We payed: ", burgerOrder.getPrice());
+console.log("We get: ", burgerOrder.getCal());
+
+const burgerOrder2 = new FishBurger("medium");
+burgerOrder.addTopping(TOPPINGS.cheese);
+burgerOrder.addTopping(TOPPINGS.spices);
+burgerOrder.addTopping(TOPPINGS.salmon);
+
+console.log("We payed: ", burgerOrder.getPrice());
+console.log("We get: ", burgerOrder.getCal());
